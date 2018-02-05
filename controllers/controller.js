014749bd2f4e42a9dbbbd9fbfb67e76d8e7c7085
Glitch (seemly-effect) <none>
2018-02-05 22:44:30 +0000

    🌶⚓️ Checkpoint controllers/controller.js:576250/520 routes/index.js:576250/141

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,7 +6,14 @@ const multer = require("multer");
 exports.homePage = (req,res) => {
    res.render("index", {title:"File MetaData"});
 }
+
 exports.upload = multer().single("file");
-exports.upload = (req, res)=>{
-res.send("Hello");
+
+exports.fileSize =(req,res) => {
+  const result = {
+    size:req.file.size,
+    fileName: req.file.originalname
+  }
+  console.log(req.file.mimetype);
+  res.json(result);
 }
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const fs = require('fs');
 const controller = require('../controllers/controller');
 const router = express.Router();
+const multer = require("multer");
 
 router.get("/", controller.homePage);
-router.post("/", controller.upload);
+router.post("/",
+      controller.upload,
+      controller.fileSize
+    );
 module.exports = router;
