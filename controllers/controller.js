a1c95c57af2ae9d2f3106fdc5fed9e296a27737e
Glitch (seemly-effect) <none>
2018-02-05 21:40:50 +0000

    📰🍵 Checkpoint package.json:576250/228 server.js:576250/249 routes/index.js:576250/46 controllers/controller.js:576250/7

---
+++ controllers/controller.js
@@ -0,0 +1,4 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+const fetch = require("node-fetch");
+const Promise = require("es6-promise").Promise;
--- server.js
+++ server.js
@@ -2,36 +2,21 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
-
+const express = require('express');
+const app = express();
+const controller = require("./controllers/controller");
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
+app.set("views", (__dirname + "views"));
+app.set("view engine", "pug");
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", (req, res) => {
+  res.send(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
